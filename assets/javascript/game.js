dc7f66e0d0ca9b95c7e6507cb73d63aeb6b1e4af
KirdMNKY <mr.mike.soto@gmail.com>
2018-07-30 20:07:49 -0500

    added questions and grid layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,93 @@
+/*
+    Trivia Game
+   - Mike Soto -
+*/
+/*
+{
+    q: ,
+    a1: ,
+    a2: ,
+    a3: ,
+    a4: ,
+},
+*/
+
+$(document).ready(function() {
+
+    var triviaQuestions = [
+        {
+            q: "What are \"Durin's Folk\" more commonly known as?", 
+            a1: "Hobbits from outside the shire",
+            a2: "Dwarves", //correct answer
+            a3: "Forest Elves",
+            a4: "Great Eagles",
+        },
+        {
+            q: "What was teh bridge of Khazad-D&ucirc;m?", 
+            a1: "The bridge over the Brandywine River, marking the end of The Shire.",
+            a2: "The mythical gateway between Middle-Earth and Valinor, the land of the gods.", 
+            a3: "The name of the broken sword before it was reforged as teh legendary And&uacute;ril.",
+            a4: "The bridge inside the Great Gates of Moria where Gandalf faced the Balrog.", //corect answer
+        },
+        {
+            q: "What kind of creatures are the spawn of Ungoliant?", 
+            a1: "Hill Giants",
+            a2: "Uruk-hai", 
+            a3: "Giant Spiders", //correct answer
+            a4: "Flying Fell Beasts", 
+        },
+        {
+            q: "Which of these is not a public inn in Middle-Earth?",
+            a1: "The Old Guesthouse" ,
+            a2: "The Green Dragon" ,
+            a3: "The Prancing Pony" ,
+            a4: "The Southern Star" , //correct answer
+        },
+        {
+            q: "Who participated in The Battle of Isengard?" ,
+            a1: "Saurumon's forces versus King Th&eacute;odin's Rohirrim." ,
+            a2: "Saurumon's forces versus the Ents" , //correct answer
+            a3: "Orcs of Dol Guldur versus the Galadhrim of Lothl&oacute;rien." ,
+            a4: "The forces of the Dark Lord Suron versus the forces of Gondor." ,
+        },
+        {
+            q: "Which of these weapons was not found in the troll's cave?" ,
+            a1: "Orcrist the Goblin-Cleaver." ,
+            a2: "Glamdring the Foe-Hammer." ,
+            a3: "Sting" ,
+            a4: "Angrist" , //correct answer
+        },
+        {
+            q: "Who ws Elanor Gaardner (also known as Elanor the Fair?" ,
+            a1: "Daughter of Samwise Gamgee and Rosie Cotton." , //correct answer
+            a2: "The Queen of Mirkwood and mother of Legolas." ,
+            a3: "One of the nine humans who was given a Ring of Power." ,
+            a4: "Wife of Bard the Bowman of Lake-Town." ,
+        },
+        {
+            q: "What did Aragorn, Imrahil, Gandalf, &Eacuteomer, Elladan, and Elrohir decide during The Last Debate?" ,
+            a1: "Who to send to destroy the Ring of Power" , 
+            a2: "Who would lead the White Council. Gandalf was elected but declined, so Saruman took over." ,
+            a3: "The number of soldiers to send to figh Sauron in the Battle of the Morannon." , //correct answer
+            a4: "How to punish the newly risen Uruk-hai" ,
+        },
+        {
+            q: "What Sindarin word was Aragorn known by when he was a child?" ,
+            a1: "Estel - meaning \"hope\" or \"trust\"." , //correct answer
+            a2: "Amdir - meaning \"looking up\"." ,
+            a3: "Aeluin - meaning \"blue lake\"." ,
+            a4: "M&iacute;riel - meaning \"sparkling like jewels." ,
+        },
+        {
+            q: "What are Morgul-Wounds?" ,
+            a1: "The corrupt mounds of earth from which Uruk-hai are born." ,
+            a2: "Wounds inflicted by the Nazg&ucirc;l." , //correct answer
+            a3: "Sieg towers used by Orcs during the attack on Minas Tirith." ,
+            a4: "The name given to an Elf who has given up its immortality." ,
+        },
+
+    ]
+
+
+
+});
