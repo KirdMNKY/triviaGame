076cebb2aae58c9a051377afb6302588718ee60d
KirdMNKY <mr.mike.soto@gmail.com>
2018-08-02 21:43:07 -0500

    added all assets and broke award screen

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,17 +2,23 @@
     Trivia Game
    - Mike Soto -
 */
-/*
-{
-    q: ,
-    a1: ,
-    a2: ,
-    a3: ,
-    a4: ,
-},
-*/
+
 
 $(document).ready(function() {
+    var score = 0;
+    var intervalId;
+    var showTimer = $("#timer");
+    var showQuestion = $("#question");
+    var showScore = $("#score");
+    var showA1 = $("#a1");
+    var showA2 = $("#a2");
+    var showA3 = $("#a3");
+    var showA4 = $("#a4");
+    var startBtn = $("#start");
+    var thirty = 30;
+    var showRightWrong = $("#rightWrong");
+    var showGIF = $("#gif");
+    var numQuestions = 0;
 
     var triviaQuestions = [
         {
@@ -23,7 +29,7 @@ $(document).ready(function() {
             a4: "Great Eagles",
         },
         {
-            q: "What was teh bridge of Khazad-D&ucirc;m?", 
+            q: "What was the bridge of Khazad-D&ucirc;m?", 
             a1: "The bridge over the Brandywine River, marking the end of The Shire.",
             a2: "The mythical gateway between Middle-Earth and Valinor, the land of the gods.", 
             a3: "The name of the broken sword before it was reforged as teh legendary And&uacute;ril.",
@@ -82,12 +88,303 @@ $(document).ready(function() {
             q: "What are Morgul-Wounds?" ,
             a1: "The corrupt mounds of earth from which Uruk-hai are born." ,
             a2: "Wounds inflicted by the Nazg&ucirc;l." , //correct answer
-            a3: "Sieg towers used by Orcs during the attack on Minas Tirith." ,
+            a3: "Siege towers used by Orcs during the attack on Minas Tirith." ,
             a4: "The name given to an Elf who has given up its immortality." ,
         },
 
     ]
 
+    var winQuotes = ["The dark fire will not avail you, Flame of Udûn!", "Hobbits really are amazing creatures.", "I have nothing greater to give than the gift you already bear.", "There is only one Lord of the Ring.", "His strength returns.", "Take some rest. These borders are well protected."];
+    var loseQuotes = ["Fool of a Took! Throw yourself in next time, and rid us of your stupidity!", "Go back to the shadow. You shall not pass!", "Victory was near, but the power of the Ring could not be undone.", "The hearts of Men are easily corrupted.", "Concealed within his fortress, the Lord of Mordor sees all.", "Against the power of Mordor, there can be no victory. ", "You have a stout heart, little Hobbit, but that will not save you."];
+
+    var winSounds = ["arwen_defeatevil.wav", "clever.wav", "galariel_small.wav", "gandalf_runshadowfax.wav", "gollum_musthaveprecious.wav", "saruman_rulemiddleearth.wav"];
+    var loseSounds = ["galadriel_task.wav", "gandalf_allends.wav", "gandalf_shallnotpass.wav", "gollum_nobodylikesyou.wav", "saruman_lesserson.wav", "saruman_removethose.wav", "treebeard_nocurse.wav"];
+
+    var winGIFs = ["win0.gif", "win1.gif", "win2.gif", "win3.gif", "win4.gif", "win5.gif", "win6.gif", "win7.gif", "win8.gif", "win9.gif"];
+    var loseGIFs = ["lose0.gif", "lose1.gif", "lose2.gif", "lose3.gif", "lose4.gif", "lose5.gif", "lose6.gif", "lose7.gif", "lose8.gif", "lose9.gif", "lose.gif"];
+
+    var correctAnswers = ["a2", "a4", "a3", "a4", "a2", "a4", "a1", "a3", "a1", "a2"];
+    var idClicked = "";
+    var i = 0;
+    var continueGame = true;
+    var correct = false;
+
+startBtn.on("click", function(){
+    i = 0;
+
+    startGame(i);
+
+})
 
 
-});
+
+function startGame(){
+    
+    resetGame();
+    showScore.text(score);
+    showQuestion.html(triviaQuestions[i].q);
+    showA1.html(triviaQuestions[i].a1);
+    showA2.html(triviaQuestions[i].a2);
+    showA3.html(triviaQuestions[i].a3);
+    showA4.html(triviaQuestions[i].a4);
+    
+    
+}
+
+function resetGame(){
+    showScore.attr("style", "display: none;");
+    startBtn.attr("style", "display: none;");
+    showQuestion.attr("style", "display: block;");
+    showA1.attr("style", "display: block;");
+    showA2.attr("style", "display: block;");
+    showA3.attr("style", "display: block;");
+    showA4.attr("style", "display: block;");
+    showTimer.attr("style", "display: inline-block;");
+    showGIF.attr("style", "display: none;");
+    showRightWrong.attr("style", "display: none;");
+    
+    console.log("reset");
+    stop();
+    idClicked = "";
+    thirty = 30;
+    showTimer.text(thirty);
+    setTimeout(questionTimer,1000);
+}
+
+
+showA1.on("click", function(){
+    idClicked = showA1.attr("id");
+    checkWin();   
+})
+showA2.on("click", function(){
+    idClicked = showA2.attr("id");
+    checkWin();   
+})
+showA3.on("click", function(){
+    idClicked = showA3.attr("id");
+    checkWin(); 
+})
+showA4.on("click", function(){
+    idClicked = showA4.attr("id");
+    checkWin();
+})
+    
+
+
+function checkWin(){
+    if(numQuestions <= answers.length){
+        if(i >= answers.length){
+            gameOver();
+        }
+        if(idClicked === correctAnswers[i]){
+            i++;
+            numQuestions++;
+            correct = true;
+            winner();
+                    
+        }
+        else {
+            i++;
+            numQuestions++;
+            correct = false;
+            loser();
+
+        }
+
+    }else {
+        gameOver();
+    }
+}
+
+function winner(){
+    score++;
+    alert("you win");
+    awardScreen();
+    
+    
+}
+
+function loser(){
+    alert("you lose");
+    awardScreen();
+    
+}
+
+function outtaTime(){
+    alert("you are out of time");
+    timeUp();
+}
+
+function timeUp(){
+    stop();
+    
+    showGIF.attr("style", "display: inline-block;");
+    //setTimeout(scoreScreenTimer,1000);
+    //clear question and answers
+    showQuestion.attr("style", "display: none;");
+    showA1.attr("style", "display: none;");
+    showA2.attr("style", "display: none;");
+    showA3.attr("style", "display: none;");
+    showA4.attr("style", "display: none;");
+    showTimer.attr("style", "display: none;");
+
+    showRightWrong.attr("style", "display: inline-block;");  
+    showRightWrong.text("We've run out of time...");
+    var randGIF = Math.floor(Math.random()*loseGIFs.length); 
+    showGIF.html("<img src=" + "\"assets/images/" + loseGIFs[randGIF] + "\">");
+    var randSound = Math.floor(Math.random()*loseSounds.length);
+    var sound = new Audio();
+    sound.src = "assets/sounds/lose/" + loseSounds[randSound];
+    sound.play();
+    setTimeout(function(){
+        startGame();
+    }, 8000);
+    stop();
+}
+
+function gameOver(){
+    //Display Game Over Screen
+    showScore.attr("style", "display: inline-block;");
+    showScore.text(score);
+    continueGame = false;
+    numQuestions = 0;
+    i = 0;
+    stop();
+    clearInterval(intervalId);
+    showGIF.attr("style", "display: inline-block;");
+    //setTimeout(scoreScreenTimer,1000);
+    //clear question and answers
+    showQuestion.attr("style", "display: none;");
+    showA1.attr("style", "display: none;");
+    showA2.attr("style", "display: none;");
+    showA3.attr("style", "display: none;");
+    showA4.attr("style", "display: none;");
+    showTimer.attr("style", "display: none;");
+    showRightWrong.text("It's finished...."); 
+    if(score >= 7){
+        showGIF.html("<img src=\"assets/images/finishwin.gif\" ");
+    }else{
+        showGIF.html("<img src=\"assets/images/finishlose.gif\" ");
+    }
+    setTimeout(function(){
+        startGame();
+    }, 8000);
+    stop();
+}
+
+function awardScreen(){
+    stop();
+    // showScore.attr("style", "display: inline-block;");
+    // showScore.text(score);
+    showGIF.attr("style", "display: inline-block;");
+    //setTimeout(scoreScreenTimer,1000);
+    //clear question and answers
+    showQuestion.attr("style", "display: none;");
+    showA1.attr("style", "display: none;");
+    showA2.attr("style", "display: none;");
+    showA3.attr("style", "display: none;");
+    showA4.attr("style", "display: none;");
+    showTimer.attr("style", "display: none;");
+
+    //show award elements
+    showScore.text(score);
+    
+
+    if(!correct){
+        showRightWrong.attr("style", "display: inline-block;");
+        var randQuote = (Math.floor(Math.random()*loseQuotes.length));  
+        showRightWrong.text(loseQuotes[randQuote]);
+        var randGIF = Math.floor(Math.random()*loseGIFs.length); 
+        showGIF.html("<img src=" + "\"assets/images/" + loseGIFs[randGIF] + "\">");
+        var randSound = Math.floor(Math.random()*loseSounds.length);
+        var sound = new Audio();
+        sound.src = "assets/sounds/lose/" + loseSounds[randSound];
+        sound.play();
+        
+    }
+    else {
+        showRightWrong.attr("style", "display: inline-block;");
+        var randQuote = (Math.floor(Math.random()*winQuotes.length));
+        showRightWrong.text(winQuotes[randQuote]);
+        var randGIF = Math.floor(Math.random()*winGIFs.length);
+        showGIF.html("<img src=\"" + "assets/images/" + winGIFs[randGIF] + "\">");
+        var randSound = Math.floor(Math.random()*winSounds.length);
+        var sound = new Audio();
+        sound.src = "assets/sounds/win/" + winSounds[randSound];
+        sound.play();
+        score++;
+    }
+    setTimeout(function(){
+        startGame();
+    }, 8000);
+    stop();
+    
+
+}
+
+
+function questionTimer() {
+  if(thirty <= 0){
+    
+    clearInterval(intervalId);
+    thirty = 30;
+    
+    return;
+
+  }
+  clearInterval(intervalId);
+  intervalId = setInterval(decrement, 1000);
+} // End of questionTimer
+
+// function scoreScreenTimer() {
+//     //gameover
+//     if(continueGame === true){
+//         thirty = 15
+//         if(thirty <= 0){
+//         clearInterval(intervalId);
+//         thirty = 30;
+//         return;
+//         }
+    
+//         clearInterval(intervalId);
+//         intervalId = setInterval(decrement, 1000);
+        
+//     }
+//     //award screen
+//     else {
+//         thirty = 15
+//         if(thirty <= 0){
+//         clearInterval(intervalId);
+//         thirty = 30;
+//         return;
+//         }
+    
+//         clearInterval(intervalId);
+//         intervalId = setInterval(decrement, 1000);
+        
+//     }
+//   } // End of scoreScreenTimer
+
+function decrement() {
+
+  thirty--;
+
+  showTimer.text(thirty);
+
+  if (thirty === 0) {
+    i++;
+    outtaTime();
+    stop();
+
+  }
+} //End of decrement
+
+function stop() {
+    
+  clearInterval(intervalId);
+} // end of stop
+
+
+
+
+}) //end of document ready
